65e562e053041e381bf0112253dc77fc2e482c47
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2022-04-19 23:50:29 +0300

    css modules

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Task.module.css'
 
 const Task = ({task}) => {
     let {title,description} = task;
   return (
     <div>
-      <h3 className="task-slider__header-title">{title}</h3>
-      <p className="task-slider__header-description">{description}</p>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
     </div>
   );
 }
